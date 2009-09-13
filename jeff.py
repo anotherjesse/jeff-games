be3df8b3def43f48785b0e289bae257e2ba6929d
Jesse Andrews <anotherjesse@gmail.com>
2009-09-12 18:31:42 -0700

    more work

--- jeff.py
+++ jeff.py
@@ -1,3 +1,5 @@
+from twisted.web import server, resource
+from twisted.internet import reactor
 import pygame
 from pygame.locals import *
 
@@ -14,7 +16,7 @@ class Shot(pygame.sprite.Sprite):
     def render(self, screen):
         pygame.draw.rect(screen,
                          [255, 0, 0],
-                         [8 + self.x*64, 8 + self.y*48,
+                         [8 + self.y*64, 8 + self.x*48,
                           48, 32])
         self.age -= 1
 
@@ -25,7 +27,7 @@ class Board():
     def __init__(self):
         self.bkg = pygame.image.load("body.png")
         self.head = pygame.image.load("head.png")
-        font = pygame.font.SysFont(pygame.font.get_default_font(), 36)
+        font = pygame.font.SysFont(pygame.font.get_default_font(), 24)
         self.digits = [font.render(str(digit), True, [255,255,255]) for digit in xrange(10)]
 
         self.boxes = []
@@ -35,17 +37,22 @@ class Board():
         screen.blit(self.bkg, [0,0])
 
         for digit in xrange(10):
-            screen.blit(self.digits[digit], [12,12+48*digit])
             pygame.draw.line(screen,
                              [255,255,0],
                              [0,48*digit],
                              [640,48*digit])
 
-            screen.blit(self.digits[digit], [12+64*digit, 12])
             pygame.draw.line(screen,
                              [255,255,0],
                              [64*digit, 0],
                              [64*digit, 480])
+        for row in xrange(10):
+            for col in xrange(10):
+                screen.blit(self.digits[col],
+                            [2+64*row, 2+48*col])
+                screen.blit(self.digits[row],
+                            [12+64*row, 2+48*col])
+
 
         for b in self.boxes:
             b.render(screen)
@@ -53,13 +60,30 @@ class Board():
         self.boxes = [b for b in self.boxes if b.alive()]
 
     def shoot(self, who, x, y):
+        print 'shot!'
         self.boxes.append(Shot(who,x,y))
 
     def run(self):
-        self.shoot('1324',3,3)
         while pygame.event.poll().type != KEYDOWN:
             self.draw(screen)
             pygame.display.update()
+            reactor.runUntilCurrent()
+            reactor.doIteration(0)
 
-board = Board()
-board.run()
+class CommandCollector(resource.Resource):
+    isLeaf = True
+
+    def __init__(self, board):
+        self.board = board
+    def render(self, req):
+        uid = req.args.get("uid",["????"])[0]
+        x = int(req.args.get("x",[0])[0])
+        y = int(req.args.get("y",[0])[0])
+        self.board.shoot(uid, x, y)
+        return "orders received, sir"
+
+if __name__ == '__main__':
+    board = Board()
+    collector = CommandCollector(board)
+    reactor.listenTCP(8080, server.Site(collector))
+    board.run()
